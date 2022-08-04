1cec376a07928025130e7c281892c885a92f74a4
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-08-04 16:51:34 +0500

    added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let elButton= document.querySelector(".header__button")
+let elBurger= document.querySelector(".header__brg")
+let elNav= document.querySelector(".header__navbar")
+let elCross= document.querySelector(".navbar__cross1")
+let elbg = document.querySelector(".header")
+elButton.addEventListener("click" , ()=> {
+    elNav.classList.toggle("active")
+    elCross.style.display = "flex"
+    elbg.classList.toggle("active__bg")
+    elBurger.classList.toggle("deactive")
+})
